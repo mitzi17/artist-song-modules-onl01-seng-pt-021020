015cb18019456a8fc4eeb718f56553d1cb3fb237
Mitzi Estrada <m_estrada17@live.com>
2020-03-13 05:29:24 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,12 +2,9 @@ module Findable
   module ClassMethods
   
   def find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.detect{|a| a.name == name}
   end
   
-  def find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
   end
 end
 end
